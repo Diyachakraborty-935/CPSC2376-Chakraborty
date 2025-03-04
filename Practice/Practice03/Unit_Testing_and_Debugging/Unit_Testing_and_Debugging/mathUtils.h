c2affce240bddef48ea133f64b34111a5da3fa94
Diyachakraborty-935 <Diyaclassicalmusic@gmail.com>
2025-03-04 16:41:41 -0600

    Practice03 Submission

--- Practice/Practice03/Unit_Testing_and_Debugging/Unit_Testing_and_Debugging/mathUtils.h
+++ Practice/Practice03/Unit_Testing_and_Debugging/Unit_Testing_and_Debugging/mathUtils.h
@@ -1,9 +1,11 @@
 // mathUtils.h
 #pragma once
-
+#include <vector>
 class mathUtils {
 public:
     static int sumRange(int start, int end);
-    static bool containsNegative(const std::vector<int>& numbers);
-    static 	static int findMax(const std::vector<int>& numbers);
+    int findMax(const std::vector<int>& numbers);
+
+    bool containsNegative(const std::vector<int>& numbers);
+
 };
